b71a3b4c32cd1e45f23bcc2341248bbfd6861015
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2018-11-21 20:32:58 -0300

    Testes de receptores finalizados

--- src/testes/UsuarioDoadorTest.java
+++ src/testes/UsuarioDoadorTest.java
@@ -22,14 +22,17 @@ class UsuarioDoadorTest {
 	
 	@Test
 	void toStringDoadorTest() {
-		assertEquals(ud.toString(), "Liu Kang/012.345.678-99, liu@hotmail.com, 1234-5678, status: doador");	
+		String formatado = "Liu Kang/01234567899, liu@hotmail.com, 1234-5678, status: doador";
+		assertEquals(ud.toString(), formatado );
 		
 	}
 	
 	@Test
 	void EqualsDoadorTest() {
-		assertEquals(ud.equals(new UsuarioDoador("Kung Lao", "kung@hotmail.com", "9876-5432", "igreja", "012.345.678-99" )), true);
-		assertEquals(ud.equals(new UsuarioDoador("Liu Kang", "liu@hotmail.com", "1234-5678", "pessoa fisica", "666.666.666-66" )), false);
+		UsuarioDoador ud2 = new UsuarioDoador("Kung Lao", "kung@hotmail.com", "9876-5432", "igreja", "012.345.678-99" );
+		assertEquals(ud, ud2);
+		UsuarioDoador ud3 = new UsuarioDoador("Liu Kang", "liu@hotmail.com", "1234-5678", "pessoa fisica", "666.666.666-66" );
+		assertEquals(ud.equals(ud3), false);
 	}
 		
 
